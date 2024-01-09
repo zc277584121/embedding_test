99b3865d67da80d1e8c942e37544a498d9a33264
ChengZi <chen.zhang@zilliz.com>
2024-01-09 19:36:18 +0800

    update voyage
    Signed-off-by: ChengZi <chen.zhang@zilliz.com>

--- models/voyage_service.py
+++ models/voyage_service.py
@@ -1,6 +1,4 @@
 import os
-import requests
-import json
 
 from models.base import ModelService
 
@@ -10,41 +8,29 @@ class VoyageService(ModelService):
     https://docs.voyageai.com/embeddings/
     """
 
-    def __init__(self, model_name='voyage-01'):
+    def __init__(self, model_name='voyage-02'):
         self.model_name = model_name
         self.voyageai_api_key = os.getenv('VOYAGE_API_KEY')
 
         import voyageai
         voyageai.api_key = self.voyageai_api_key
+        self.vo = voyageai.Client()
 
     def get_embedding(self, text, input_type='document'):
         """
             input_type (`str`): "query" or "document" for retrieval case.
         """
-        from voyageai import get_embeddings
-        import voyageai
-        voyageai.api_key = self.voyageai_api_key
-
         documents = [text]
-        embeddings = get_embeddings(documents, model=self.model_name, input_type=input_type)
-        return embeddings[0]
+        embeddings = self.vo.embed(documents, model=self.model_name, input_type=input_type)
+        return embeddings.embeddings[0]
 
     def get_token_num(self, text):
-        url = "https://api.voyageai.com/v1/embeddings"
-        headers = {
-            "Content-Type": "application/json",
-            "Authorization": f"Bearer {self.voyageai_api_key}"
-        }
-        data = {
-            "input": text,
-            "model": self.model_name
-        }
-        response = requests.post(url, headers=headers, data=json.dumps(data))
-        result = response.json()
-        return result['usage']['total_tokens']
+        total_tokens = self.vo.count_tokens([text])
+        return total_tokens
 
 
 if __name__ == '__main__':
     voyage_service = VoyageService()
-    res = voyage_service.get_token_num('hello')
+    res = voyage_service.get_embedding('hello')
+    # res = voyage_service.get_token_num('hello world')
     print(res)
